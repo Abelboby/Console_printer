d3297e55ac90d9c4d881fccb06c088158a28e09b
Abelboby <abeldavisboby@gmail.com>
2025-02-04 17:53:06 +0530

    Remove legacy socket and Flask implementations for lyrics display

--- lyrics.ps1
+++ lyrics.ps1
@@ -4,14 +4,27 @@ Line 2
 Line 3...
 "@
 
-Clear-Host
-$lyrics -split "`n" | ForEach-Object {
-    $line = $_
-    $chars = $line.ToCharArray()
-    for($i=0; $i -lt $chars.Length; $i++) {
-        Write-Host $chars[$i] -NoNewline
-        Start-Sleep -Milliseconds 100
+$originalBg = $Host.UI.RawUI.BackgroundColor
+$originalFg = $Host.UI.RawUI.ForegroundColor
+
+try {
+    $Host.UI.RawUI.BackgroundColor = "Black"
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Clear-Host
+    
+    $lyrics -split "`n" | ForEach-Object {
+        $line = $_
+        $chars = $line.ToCharArray()
+        for($i=0; $i -lt $chars.Length; $i++) {
+            Write-Host $chars[$i] -NoNewline
+            Start-Sleep -Milliseconds 100
+        }
+        Write-Host ""
+        Start-Sleep -Seconds 2
     }
-    Write-Host ""
-    Start-Sleep -Seconds 2
+}
+finally {
+    $Host.UI.RawUI.BackgroundColor = $originalBg
+    $Host.UI.RawUI.ForegroundColor = $originalFg
+    Clear-Host
 } 
